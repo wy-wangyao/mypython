4ef13a4473130483b584b0882633c567dced30e6
WangYao <1446745072@qq.com>
2018-06-27 13:55:52 +0800

    python留言板,打包

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name='guestbook',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Flask',
+    ],
+    entry_points="""
+    [console_scripts]
+    guestbook = guestbook:main
+    """,
+)
